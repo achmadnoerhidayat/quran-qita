20fed89eaa42acda5e73340803da8e6651422c54
ahmadvanhowten17 <ahmadvanhowten17@gmail.com>
2025-08-11 23:51:05 +0700

    update view home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1521,8 +1521,7 @@
     @endif
 </head>
 
-<body
-    class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
+<body class="flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
     <p>
         Quran'Qita
     </p>
