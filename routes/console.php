8cd04dbe31b323e292bc26f3df7d6766bfd12e97
ahmadvanhowten17 <ahmadvanhowten17@gmail.com>
2025-09-25 10:08:15 +0700

    update job

--- routes/console.php
+++ routes/console.php
@@ -9,4 +9,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command(CheckExpiredSubscriptions::class)->everyMinute();
+Schedule::command(CheckExpiredSubscriptions::class)->daily();
