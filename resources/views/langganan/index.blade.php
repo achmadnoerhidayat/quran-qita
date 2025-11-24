e31db100bb4d4f149a30b978a61cb9a05d8ce81c
ahmadvanhowten17 <ahmadvanhowten17@gmail.com>
2025-11-24 16:27:33 +0700

    bug langganan

--- resources/views/langganan/index.blade.php
+++ resources/views/langganan/index.blade.php
@@ -100,10 +100,10 @@
                                         @if ($news->status === 'expired')
                                             <span
                                                 class="bg-red-100 text-red-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-red-900 dark:text-red-300">{{ $news->status }}</span>
-                                        @elseif ($news->status === 'cancelled')
+                                        @elseif ($news->status === 'failed')
                                             <span
                                                 class="bg-red-100 text-red-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-red-900 dark:text-red-300">{{ $news->status }}</span>
-                                        @elseif ($news->status === 'active')
+                                        @elseif ($news->status === 'success')
                                             <span
                                                 class="bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-green-900 dark:text-green-300">{{ $news->status }}</span>
                                         @elseif ($news->status === 'pending')
